f84641cee5eea04adf5bec11d42beac902e3ef09
Madlax97 <67518337+Madlax97@users.noreply.github.com>
2021-01-21 13:34:27 -0700

    Add files via upload

---
+++ CellPhoneTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package docComments;
+
+/**
+ * Class that tests the CellPhone class. 
+ * 
+ * 
+ * @author brandon
+ *
+ */
+public class CellPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CellPhone myPhone = new CellPhone("Iphone", 2018, false);
+		
+		
+		System.out.println(myPhone.toString());
+		
+	}
+
+}
